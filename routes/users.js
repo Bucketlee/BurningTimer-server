7c19bdcde4d24758474b6eabade233086480b939
Chanyoung <chanyoung.cedu@gmail.com>
2022-02-24 17:28:30 +0900

    feat(router): connect users router

---
+++ routes/users.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+const { login, signup, findId, findPassword } = require("../controllers/index");
+
+const router = Router();
+
+router.post("/login", login);
+router.post("/signup", signup);
+router.post("/help/id", findId);
+router.post("/help/pw", findPassword);
+
+module.exports = router;
