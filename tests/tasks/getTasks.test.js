6d703b9442c1cd1962b50d2ffc52a10ca4484cf6
Chanyoung <chanyoung.cedu@gmail.com>
2022-03-01 22:37:59 +0900

    test(user): make getTasks.test.js

---
+++ tests/tasks/getTasks.test.js
@@ -0,0 +1,35 @@
+const request = require("supertest");
+const app = require("../../index");
+const mongoose = require("mongoose");
+
+describe("Node Server Request getTasks Function.", () => {
+  it("Should answer GET requests for /api/tasks with a 200 status code", async () => {
+    // given
+    // username = test;
+    const token = "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QiLCJpYXQiOjE2NDEwMjk3MjUsImV4cCI6MTY0MzYyMTcyNX0.6V0c77TqbpElBshZYWTXGhS4dWPdbhyBsXyYlt-0yfw";
+
+    // when
+    const { body, statusCode } = await request(app).get("/api/tasks").set("authorization", token);
+
+    // then
+    expect(statusCode).toEqual(200);
+    expect(body.message).toEqual("Ok");
+  });
+
+  it("Should 401 when ID isn\'t exists", async () => {
+    // given
+    // username = testtest
+    const token = "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3R0ZXN0IiwiaWF0IjoxNjQxMDI5NzI1LCJleHAiOjE2NDM2MjE3MjV9.LzPxS16UfK1cKABQcm3KjS44I0Cn_LGw8k-6qIau1uQ";
+
+    // when
+    const { body, statusCode } = await request(app).get("/api/tasks").set("authorization", token);
+
+    // then
+    expect(statusCode).toEqual(401);
+    expect(body.message).toEqual("Not Authorized");
+  });
+
+  done();
+})
+
+afterAll(() => mongoose.disconnect());
