783feb033b55c8356d36fda52e1c6f2cf7c6b955
Chanyoung <chanyoung.cedu@gmail.com>
2022-03-01 22:37:22 +0900

    test(user): make getCategories.test.js

---
+++ tests/categories/getCategories.test.js
@@ -0,0 +1,35 @@
+const request = require("supertest");
+const app = require("../../index");
+const mongoose = require("mongoose");
+
+describe("Node Server Request getCategories Function.", () => {
+  it("Should answer GET requests for /api/categories with a 200 status code", async () => {
+    // given
+    // username = test;
+    const token = "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QiLCJpYXQiOjE2NDEwMjk3MjUsImV4cCI6MTY0MzYyMTcyNX0.6V0c77TqbpElBshZYWTXGhS4dWPdbhyBsXyYlt-0yfw";
+
+    // when
+    const { body, statusCode } = await request(app).get("/api/categories").set("authorization", token);
+
+    // then
+    expect(statusCode).toEqual(200);
+    expect(body.message).toEqual("Ok");
+  });
+
+  it("Should 401 when ID isn\'t exists", async () => {
+    // given
+    // username = test;
+    const token = "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QiLCJpYXQiOjE2NDEwMjk3MjUsImV4cCI6MTY0MzYyMTcyNX0.6V0c77TqbpElBshZYWTXGhS4dWPdbhyBsXyYlt-0yfw";
+
+    // when
+    const { body, statusCode } = await request(app).get("/api/categories").set("authorization", token);
+
+    // then
+    expect(statusCode).toEqual(401);
+    expect(body.message).toEqual("Not Authorized");
+  });
+
+  done();
+})
+
+afterAll(() => mongoose.disconnect());
