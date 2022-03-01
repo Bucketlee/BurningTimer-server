2e6f25d5cf5debfc8d85c2b4c9cf549ed35c022c
Chanyoung <chanyoung.cedu@gmail.com>
2022-03-01 22:49:31 +0900

    feat(db): make category model

---
+++ models/category.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const user = require("./user");
+
+const Schema = mongoose.Schema;
+
+const category = new Schema(
+  {
+    userId: { type: String, required: true, ref: user },
+    name: { type: String, required: true },
+    priority: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Category", category);
