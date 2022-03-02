5a4e6fc928028806a6135d2b7449327d89b2b878
Chanyoung <chanyoung.cedu@gmail.com>
2022-03-03 06:07:59 +0900

    feat(controllers): make getCategories controller

---
+++ controllers/categories/getCategories.js
@@ -0,0 +1,14 @@
+const Category = require("../../models/category");
+const User = require("../../models/user");
+
+module.exports = async function getCategories(req, res) {
+  const username = req.token;
+
+  try {
+    const user = await User.findOne({ username });
+    const categories = await Category.find({ userId: user._id });
+    return res.status(200).send({ data: categories, message: "Ok" });
+  } catch (err) {
+    return res.status(500).send({ message: "Unexpected server error." });
+  }
+};
