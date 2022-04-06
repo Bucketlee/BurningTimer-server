ead5b2f0a00d704d2d70f11697baf70f24dae1e4
Chanyoung <chanyoung.cedu@gmail.com>
2022-04-07 02:04:28 +0900

    ci: connect static files

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 const router = require("./routes");
@@ -19,9 +20,7 @@ app.use(
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use(express.static(path.join(__dirname, "static")));
 
 app.use("/api", router);
 
